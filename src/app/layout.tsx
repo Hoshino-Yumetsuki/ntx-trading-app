561586f5d07a0176e113f966876aebdc4c1eb69f
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2025-07-18 13:14:00 +0800

    refactor: 修改网站标题和描述

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev'
+  title: 'NTX Trading | 掌握机构操盘体系，提升你的交易能力',
+  description: '掌握机构操盘体系，提升你的交易能力'
 }
 
 export default function RootLayout({
